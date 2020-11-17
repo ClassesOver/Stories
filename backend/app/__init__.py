0686403037ec46de64964e8f895a599a6f2b9f3c
zhaoxiangjun <xiangjun.zhao@hand-china.com>
2020-11-17 17:53:46 +0800

    fix import error

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -66,7 +66,7 @@ babel = Babel()
 
 
 def create_app(config_class=Config):
-    app = Flask(__name__)
+    app = Flask(__name__, static_folder='../../frontend/build', static_url_path='/')
     app.config.from_object(config_class)
     
     db.init_app(app)
@@ -86,12 +86,6 @@ def create_app(config_class=Config):
 
     from app.errors import bp as errors_bp
     app.register_blueprint(errors_bp)
-    
-    from app.auth import bp as auth_bp
-    app.register_blueprint(auth_bp, url_prefix='/auth')
-    
-    from app.main import bp as main_bp
-    app.register_blueprint(main_bp)
     
     from app.api import bp as api_bp
     app.register_blueprint(api_bp, url_prefix='/api')
--- backend/microblog.py
+++ backend/microblog.py
@@ -1,9 +1,13 @@
 from app import create_app, db, cli
 from app.models import User, Post, Message, Notification, Task, Tag, Comment
-
+from flask import render_template
 app = create_app()
 cli.register(app)
 
+
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 @app.shell_context_processor
 def make_shell_context():
